562d0d0d7f7544c7b114550fcfb22a3be51dcd13
Misayo Suzuki <msuzuki07@mylangara.ca>
2026-06-08 12:38:12 -0700

    created color pallet

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -32,7 +32,7 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Home" component={HomeStack} options={{ headerShown: false }}/>
         <Tab.Screen name="Journal" component={JournalScreen} />
       </Tab.Navigator>
     </NavigationContainer>
