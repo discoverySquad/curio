2d0d6c54f09086908b70d3ea972634fdc51867d4
Mie Takahashi <mietakahashi1993@gmail.com>
2026-06-09 17:40:10 -0700

    add choose activity logic

--- mobile/src/screens/ActivityDescription.js
+++ mobile/src/screens/ActivityDescription.js
@@ -1,13 +1,50 @@
-import { View, Text, StyleSheet, Pressable, Button } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Button, Image} from 'react-native'
+import {useEffect, useState} from 'react'
 import React from 'react'
 
 import CustomButton from '../components/CustomButton.js'
 
-const ActivityDescription = ({ navigation }) => {
+const ActivityDescription = ({ navigation, route }) => {
+  const categoryName = route?.params?.categoryName;
+    const [task, setTask] = useState(null);
+
+    useEffect(() => {
+        fetchRandomTask();
+    }, []);
+
+
+  const fetchRandomTask = async () => {
+  try {
+    const url = `${process.env.EXPO_PUBLIC_API_URL}/api/category/name/${categoryName}`;
+    console.log('url:', url);
+    console.log('categoryName:', categoryName);
+
+    const res = await fetch(url);
+    console.log('status:', res.status);
+
+    const text = await res.text();
+    console.log('response:', text); 
+
+    const data = JSON.parse(text);
+    const items = data.categoryItem;
+    console.log('items:', JSON.stringify(items)); 
+    console.log('items.length:', items.length); 
+
+    const randomItem = items[Math.floor(Math.random() * items.length)];
+    console.log('randomItem:', randomItem);   
+
+    setTask(randomItem);
+
+  } catch (error) {
+    console.error('Error:', error); // ← 必ずエラーを表示
+  }
+};
+
   return (
     <View style={styles.container}>
-        <Text style={styles.h1}>Tasks show here</Text>
-
+        <Text style={styles.h1}>{task?.task}</Text>
+        {task?.img ? (<Image source={{ uri: task.img }} style={styles.taskImage}/>) : null}
+        <Text>{task?.description}</Text>
         
         <View style={styles.tipContainer}>
             <Text>Explore safety and ask an adult if needed.</Text>
@@ -38,10 +75,11 @@ const styles= StyleSheet.create({
       fontSize: 28,
       textAlign:"center"
     },
-    imgContainer:{
-        height:200,
-        justifyContent:"center",
-        backgroundColor:"#a6a5a5"
+    taskImage: {
+        width: 300,
+        height: 300,
+        borderRadius: 16,
+        marginVertical: 16,
     },
     tipBox:{
         marginVertical:20
