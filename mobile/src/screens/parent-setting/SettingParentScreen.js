0cdbd2ed2f10a16240a13b82dc21c300523f2be6
Mie Takahashi <mietakahashi1993@gmail.com>
2026-07-01 18:37:01 -0700

    more ui update

--- mobile/src/screens/parent-setting/SettingParentScreen.js
+++ mobile/src/screens/parent-setting/SettingParentScreen.js
@@ -459,7 +459,7 @@ editRow: {
 },
   modalOverlay: {
     flex: 1,
-    backgroundColor: "#888",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     justifyContent: "center",
     alignItems: "center",
   },
